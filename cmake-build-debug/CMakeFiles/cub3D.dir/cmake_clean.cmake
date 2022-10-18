40b947d52bed624f63a208efe4d298d4f8e7a913
Roman Mazuritskiy <rmazurit@2-D-3.42heilbronn.de>
2022-10-18 13:12:23 +0200

    a del

---
+++ cmake-build-debug/CMakeFiles/cub3D.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cub3D.dir/src/main.c.o"
+  "CMakeFiles/cub3D.dir/src/main.c.o.d"
+  "cub3D"
+  "cub3D.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cub3D.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
