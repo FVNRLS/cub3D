108b00162108ec791b355b8b1807677cc6a689d1
Heleen Oomen <hoomen@student.42heilbronn.de>
2022-10-18 13:36:52 +0200

    program

--- cmake-build-debug/CMakeFiles/cub3D.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/cub3D.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/Users/rmazurit/Documents/42_Projects/cub3D/src/main.c" "CMakeFiles/cub3D.dir/src/main.c.o" "gcc" "CMakeFiles/cub3D.dir/src/main.c.o.d"
+  "/Users/hoomen/CLionProjects/cub3D/src/main.c" "CMakeFiles/cub3D.dir/src/main.c.o" "gcc" "CMakeFiles/cub3D.dir/src/main.c.o.d"
   )
 
 # Targets to which this target links.
