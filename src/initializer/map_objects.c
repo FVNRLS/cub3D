752f19be897c12fe95e3c523c5caeb97c024e0d3
Roman Mazuritskiy <rmazurit@2-D-3.42heilbronn.de>
2022-10-25 11:24:15 +0200

    ready to merge to main

--- src/initializer/map_objects.c
+++ src/initializer/map_objects.c
@@ -6,7 +6,7 @@
 /*   By: rmazurit <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 15:05:15 by rmazurit          #+#    #+#             */
-/*   Updated: 2022/10/21 16:56:36 by rmazurit         ###   ########.fr       */
+/*   Updated: 2022/10/25 11:22:48 by rmazurit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 static void create_items_container(t_data *data)
 {
-	data->conf->specs[0] = S_NEWLINE;
+	data->conf->specs[0] = NEWLINE;
 	data->conf->specs[1] = NORTH;
 	data->conf->specs[2] = SOUTH;
 	data->conf->specs[3] = EAST;
@@ -22,16 +22,6 @@ static void create_items_container(t_data *data)
 	data->conf->specs[5] = CEILING;
 	data->conf->specs[6] = FLOOR;
 	data->conf->specs[NUM_ITEMS] = NULL;
-
-	data->conf->items[0] = VOID;
-	data->conf->items[1] = ZERO;
-	data->conf->items[2] = ONE;
-	data->conf->items[3] = PLAYER_N;
-	data->conf->items[4] = PLAYER_S;
-	data->conf->items[5] = PLAYER_E;
-	data->conf->items[6] = PLAYER_W;
-	data->conf->items[7] = '\0';
-
 }
 
 static void init_config_items(t_data *data)
@@ -48,16 +38,10 @@ static void init_config_items(t_data *data)
 		print_int_error(MALLOC_ERROR, NULL);
 		ft_free_all_and_exit(data);
 	}
-	data->conf->items = malloc(sizeof(char) * (NUM_ITEMS + 1));
-	if (!data->conf->items)
-	{
-		print_int_error(MALLOC_ERROR, NULL);
-		ft_free_all_and_exit(data);
-	}
 	data->conf->line = NULL;
 	data->conf->tokens = NULL;
 	data->conf->fd = -1;
-	data->conf->line_num = 1;
+	data->conf->map_parsed = false;
 	create_items_container(data);
 }
 
