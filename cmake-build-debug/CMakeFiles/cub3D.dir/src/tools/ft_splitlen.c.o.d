c285b109ce4b4ab4fd8ed19e3ed9274ae38d5491
Heleen Oomen <hoomen@student.42heilbronn.de>
2022-11-03 13:12:39 +0100

    remove debugging stuff

--- cmake-build-debug/CMakeFiles/cub3D.dir/src/tools/ft_splitlen.c.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/cub3D.dir/src/tools/ft_splitlen.c.o: \
-  /Users/hoomen/CLionProjects/cub3D/src/tools/ft_splitlen.c
--- cmake-build-debug/CMakeFiles/cub3D.dir/src/tools/ft_strcmp.c.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/cub3D.dir/src/tools/ft_strcmp.c.o: \
-  /Users/hoomen/CLionProjects/cub3D/src/tools/ft_strcmp.c
