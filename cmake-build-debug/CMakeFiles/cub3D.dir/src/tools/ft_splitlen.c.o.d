bc8412558e145ea3fb8338a1a3c1811f4cf5bf98
Roman Mazuritskiy <rmazurit@3-B-2.42heilbronn.de>
2022-11-03 15:43:57 +0100

    remove debugging folders

--- cmake-build-debug/CMakeFiles/cub3D.dir/src/tools/ft_splitlen.c.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/cub3D.dir/src/tools/ft_splitlen.c.o: \
-  /Users/rmazurit/Documents/42_Projects/cub3D/src/tools/ft_splitlen.c
--- cmake-build-debug/CMakeFiles/cub3D.dir/src/tools/ft_strcmp.c.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/cub3D.dir/src/tools/ft_strcmp.c.o: \
-  /Users/rmazurit/Documents/42_Projects/cub3D/src/tools/ft_strcmp.c
